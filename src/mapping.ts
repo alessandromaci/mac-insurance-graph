acd52bacaf9f269f44a0b9643b8de5ec256c216c
ale_maci <alessandromaci96@gmail.com>
2022-05-13 19:17:35 +0200

    graph set-up

---
+++ src/mapping.ts
@@ -0,0 +1,45 @@
+import {
+  InsuranceRequestCreated,
+  PoolUpdated,
+} from "../generated/MacInsuranceMain/MacInsuranceMain";
+import { PoolEntity, InsuranceRequestEntity } from "../generated/schema";
+
+export function handleInsuranceRequestCreated(
+  event: InsuranceRequestCreated
+): void {
+  let insuranceRequestEntity = InsuranceRequestEntity.load(
+    event.transaction.from.toHex()
+  );
+  if (!insuranceRequestEntity) {
+    insuranceRequestEntity = new InsuranceRequestEntity(
+      event.transaction.from.toHex()
+    );
+  }
+  insuranceRequestEntity.id =
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  insuranceRequestEntity.createdAtTimestamp = event.block.timestamp;
+  insuranceRequestEntity.poolId = event.params.poolId;
+  insuranceRequestEntity.tokenAddress = event.params.tokenAddress;
+  insuranceRequestEntity.insuranceRequester = event.params.insuranceRequester;
+  insuranceRequestEntity.feeAmount = event.params.fee;
+  insuranceRequestEntity.insuranceLiquidityRequest =
+    event.params.liquidtyToInsure;
+}
+
+export function handlePoolUpdated(event: PoolUpdated): void {
+  let poolEntity = PoolEntity.load(event.transaction.from.toHex());
+  if (!poolEntity) {
+    poolEntity = new PoolEntity(event.transaction.from.toHex());
+  }
+  poolEntity.id =
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  poolEntity.createdAtTimestamp = event.block.timestamp;
+  poolEntity.poolId = event.params.poolId;
+  poolEntity.tokenAddress = event.params.tokenAddress;
+  poolEntity.basePrice = event.params.basePrice;
+  poolEntity.tresholdPrice = event.params.tresholdPrice;
+  poolEntity.feePercentage = event.params.fee;
+  poolEntity.insuranceLiquidityAdded = event.params.liquidityAdded;
+  poolEntity.startDate = event.params.startDate;
+  poolEntity.endDate = event.params.endDate;
+}
